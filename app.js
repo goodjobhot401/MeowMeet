19f1d4fe55fbaea793a27e52ea13c28611ed723e
goodjobhot401 <goodjobhot@gmail.com>
2023-07-04 15:57:06 +0800

    project init

---
+++ app.js
@@ -0,0 +1,23 @@
+// 如果環境名不是production就引入dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const routes = require('./routes')
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
+})
+
+module.exports = app
