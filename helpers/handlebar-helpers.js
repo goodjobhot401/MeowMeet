9d41b808064808546d005c3489d93ce21d5e9626
goodjobhot401 <goodjobhot@gmail.com>
2023-07-26 15:33:52 +0800

    feat: add meow-page and install day.js

--- helpers/handlebar-helpers.js
+++ helpers/handlebar-helpers.js
@@ -1,14 +1,25 @@
+const dayjs = require('./dayjs-helper')
+const relativeTime = require('dayjs/plugin/relativeTime')
+
+// 使用擴充
+// dayjs.extend(updateLocale)
+dayjs.extend(relativeTime)
+
 module.exports = {
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   },
+
+  // 依據回傳資料判定輸出 icon 數量
   generateIcons: function (number) {
     let icons = ''
     for (let i = 0; i < number; i++) {
-      icons += '<i class="fa-brands fa-gratipay" style="color: #ff5290;"></i>'
+      icons += '<i class="fa-brands fa-gratipay me-1" style="color: #ff5290;"></i>'
     }
     return icons
   },
+
+  // 依據回傳資料判定輸出 icon
   genderIcon: function (gender) {
     switch (gender) {
       case 'male':
@@ -19,6 +30,8 @@ module.exports = {
         return '<i class="fa-regular fa-circle-question" style="color: #787878;"></i>'
     }
   },
+
+  // 依據回傳資料判定輸出 icon
   neuterIcon: function (neuter) {
     switch (neuter) {
       case 'neutered':
@@ -28,5 +41,10 @@ module.exports = {
       case 'unknown':
         return '<i class="fa-regular fa-circle-question" style="color: #787878;"></i>'
     }
+  },
+
+  // 人性化時間顯示：ex. 16 hours age
+  relativeTime: function (a) {
+    return dayjs(a).fromNow()
   }
 }
--- public/index.js
+++ public/index.js
@@ -186,7 +186,7 @@ function renderSearchResults(data) {
     const genderIcon = document.createElement('i')
     genderIcon.className = 'genderIcon'
     if (meow.gender === 'female') {
-      genderIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ff5290}</style><path d="M80 176a112 112 0 1 1 224 0A112 112 0 1 1 80 176zM224 349.1c81.9-15 144-86.8 144-173.1C368 78.8 289.2 0 192 0S16 78.8 16 176c0 86.3 62.1 158.1 144 173.1V384H128c-17.7 0-32 14.3-32 32s14.3 32 32 32h32v32c0 17.7 14.3 32 32 32s32-14.3 32-32V448h32c17.7 0 32-14.3 32-32s-14.3-32-32-32H224V349.1z"/></svg>`
+      genderIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ff2590}</style><path d="M80 176a112 112 0 1 1 224 0A112 112 0 1 1 80 176zM224 349.1c81.9-15 144-86.8 144-173.1C368 78.8 289.2 0 192 0S16 78.8 16 176c0 86.3 62.1 158.1 144 173.1V384H128c-17.7 0-32 14.3-32 32s14.3 32 32 32h32v32c0 17.7 14.3 32 32 32s32-14.3 32-32V448h32c17.7 0 32-14.3 32-32s-14.3-32-32-32H224V349.1z"/></svg>`
     } else if (meow.gender === 'male') {
       genderIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#1e3150}</style><path d="M289.8 46.8c3.7-9 12.5-14.8 22.2-14.8H424c13.3 0 24 10.7 24 24V168c0 9.7-5.8 18.5-14.8 22.2s-19.3 1.7-26.2-5.2l-33.4-33.4L321 204.2c19.5 28.4 31 62.7 31 99.8c0 97.2-78.8 176-176 176S0 401.2 0 304s78.8-176 176-176c37 0 71.4 11.4 99.8 31l52.6-52.6L295 73c-6.9-6.9-8.9-17.2-5.2-26.2zM400 80l0 0h0v0zM176 416a112 112 0 1 0 0-224 112 112 0 1 0 0 224z"/></svg>`
     } else {
@@ -203,7 +203,8 @@ function renderSearchResults(data) {
 
     const li2 = document.createElement('li')
     li2.className = 'list-group-item'
-    li2.innerHTML = `<span class="text-16px-reg">親人：</span>`
+    li2.innerHTML = `<span class="text-14px-reg"><i class="fa-solid fa-paw me-1"
+      style="color: #ff5290;"></i>親人：</span>`
     const friendly = meow.friendly
     for (let i = 0; i < friendly; i++) {
       const heart = document.createElement('i')
@@ -214,7 +215,8 @@ function renderSearchResults(data) {
 
     const li3 = document.createElement('li')
     li3.className = 'list-group-item'
-    li3.innerHTML = `<span class="text-16px-reg">簡介：<br>${meow.intro}</span>`
+    li3.innerHTML = `<span class="text-14px-reg"><i class="fa-solid fa-paw me-1"
+      style="color: #ff5290;"></i>簡介：<br>${meow.intro}</span>`
 
     ul.appendChild(li1)
     ul.appendChild(li2)
