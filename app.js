59faeb8f14ec7267e312ccc4b7fa978d51772049
goodjobhot401 <goodjobhot@gmail.com>
2023-07-27 11:47:13 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 const handlebarsHelper = require('./helpers/handlebar-helpers')
 const routes = require('./routes')
 const googleApi = require('./googleApi')
@@ -15,6 +16,12 @@ const port = process.env.PORT || 3000
 app.use(express.static('public'))
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: handlebarsHelper }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(methodOverride('_method'))
